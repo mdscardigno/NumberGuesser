6c79ea210bbf60fc2fe5a6ad36ac2aa673d532ff
Milagro Scardigno <mdscardigno@gmail.com>
2021-08-08 19:27:07 -0400

    cleaning some more

--- Program.cs
+++ Program.cs
@@ -23,29 +23,36 @@ namespace NumberGuesser
                 int midValue = lowest + midpoint;
                 bool secretNumber = true;
 
-                Console.WriteLine("Think of a number between 1 to 100. Press enter to continue");
+                Console.WriteLine("Think of a number between 1 to 1024. Press enter to continue");
                 Console.ReadKey();
                 while (lowest <= highest && secretNumber)
                 {
                     differenceOfRange = highest - lowest;
+                    Console.Write("differeneOfRange", differenceOfRange);
                     midpoint = differenceOfRange / 2;
                     midValue = lowest + midpoint;
-
+                    //starting at the middle 512
                     Console.WriteLine("Is your number: {0}", midValue);
+                    //options to tell the computer if the number is too low, too high or correct
                     Console.WriteLine(" 1: No my  number is lower \n 2: No my number is greater \n 3: That is my number! ");
+                    //prompt returns a string so we have to use int.Parse to convert the string to an integer
                     int userChoiceToInt = int.Parse(Console.ReadLine());
+                    //we create a variable userChoice to store the integer
                     int userChoice = userChoiceToInt;
+                    //we check what option the user selected
                     if (userChoice == 3)
                     {
                         Console.WriteLine("You guessed it!");
-                        secretNumber = false;
+                        secretNumber = false;//the number is not longer secret
                     }
 
                     if (lowest < highest && userChoice == 2)
                     {
+                        //if number is too high
                         lowest = midValue + 1;
                         Console.WriteLine("Your number was {0}", midValue);
                     }
+                    //if number is too low
                     else if (lowest < highest && userChoice == 1)
                     {
                         highest = midValue - 1;
